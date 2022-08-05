72e6d858e999012f28fb6844aceda54d32b871c7
mrtangws <wensheng.home@gmail.com>
2022-08-06 01:00:06 +0800

    sandbox17

--- sandbox.js
+++ sandbox.js
@@ -83,7 +83,7 @@ function getCognitoIdentityCredentials(){
 
     AWS.config.credentials.get(function(err) {
         if (err){
-            logMessage(err.message);
+            logMessage("getCognitoIdentityCredentials() error" + err.message);
             window.location.replace("https://clouette.auth.ap-southeast-2.amazoncognito.com/login?client_id=528sv3n60c6h15m39lr781tn6n&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://mrtangws.github.io/");
         }
         else {
@@ -102,7 +102,7 @@ function getCurrentLoggedInSession(){
     if(cognitoUser != null){
         cognitoUser.getSession(function(err, session) {
             if (err) {
-                logMessage(err.message);
+                logMessage("getCurrentLoggedInSession() error" + err.message);
                 window.location.replace("https://clouette.auth.ap-southeast-2.amazoncognito.com/login?client_id=528sv3n60c6h15m39lr781tn6n&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://mrtangws.github.io/");
 
             }else{
@@ -114,6 +114,7 @@ function getCurrentLoggedInSession(){
         });
     }else{
         logMessage('Session expired. Please log in again.');
+        window.location.replace("https://clouette.auth.ap-southeast-2.amazoncognito.com/login?client_id=528sv3n60c6h15m39lr781tn6n&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://mrtangws.github.io/");
     }
 }
 function logMessage(message){
